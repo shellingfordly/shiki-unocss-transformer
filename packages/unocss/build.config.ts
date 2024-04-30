10e2ec33281e7d2af362a68127006725318a4fb9
shellingfordly <shellingfordly@qq.com>
2024-04-30 10:35:40 +0800

    chore: update version

--- packages/unocss/build.config.ts
+++ packages/unocss/build.config.ts
@@ -3,6 +3,7 @@ import { defineBuildConfig } from "unbuild";
 export default defineBuildConfig({
   entries: ["src/index.ts"],
   declaration: true,
+  failOnWarn: false,
   rollup: {
     emitCJS: false,
     dts: {
